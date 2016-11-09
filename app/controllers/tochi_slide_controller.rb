e82167a3e720cf1c4252ea4113dcc6791def868d
Hiroaki <a1608ho@aiit.ac.jp>
2016-11-09 12:47:22 +0000

    corrected controller

--- app/controllers/tochi_slide_controller.rb
+++ app/controllers/tochi_slide_controller.rb
@@ -1,8 +1,8 @@
 class TochiSlideController < ApplicationController
   def index
-	@users = landslide.all
-	@hash = Gmaps4rails.build_markers(@users) do |landslide, marker|
-	marker.lat landslide.latitude
+	@users = Landslide.all
+	@hash = Gmaps4rails.build_markers(@users) do |landslides, marker|
+	marker.lat landslides.latitude
 	marker.lng landsliede.longitude  end
 end
 end
