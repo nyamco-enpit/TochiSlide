b0367f1c80c90686479674ea8f44b4ee837556c2
Hiroaki <a1608ho@aiit.ac.jp>
2016-11-09 12:51:53 +0000

    deleted unnecessary s

--- app/controllers/tochi_slide_controller.rb
+++ app/controllers/tochi_slide_controller.rb
@@ -1,8 +1,8 @@
 class TochiSlideController < ApplicationController
   def index
 	@users = Landslide.all
-	@hash = Gmaps4rails.build_markers(@users) do |landslides, marker|
-	marker.lat landslides.latitude
-	marker.lng landsliede.longitude  end
+	@hash = Gmaps4rails.build_markers(@users) do |landslide, marker|
+	marker.lat landslide.latitude
+	marker.lng landslide.longitude  end
 end
 end
